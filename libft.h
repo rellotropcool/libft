4bec3bb49782be0397bdf3e17da8521f94667789
rellotropcool <rellotropcool@gmail.com>
2023-11-09 14:18:00 +0100

    update strncmp

--- libft.h
+++ libft.h
@@ -19,6 +19,7 @@ int	ft_toupper(int c);
 int	ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif
