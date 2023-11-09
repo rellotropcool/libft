b05b95868dbfeb0d7339eee2807ef8767e1646b2
rellotropcool <rellotropcool@gmail.com>
2023-11-09 17:35:12 +0100

    ajout strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if(!str)
+		return (NULL);
+	while(s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while(s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = 0;
+	return (str);
+}
