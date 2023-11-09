1a08cd2c18cd14ca6f53de3d0979108584ffca24
rellotropcool <rellotropcool@gmail.com>
2023-11-09 14:04:41 +0100

    update libft.h et Makefile

--- libft.h
+++ libft.h
@@ -14,6 +14,12 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t  ft_strlcat(char *dest, const char *src, size_t size);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif
 
