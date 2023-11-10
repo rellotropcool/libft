b120a6da926895a4b0ef332389d2e8fdc865938f
rellotropcool <rellotropcool@gmail.com>
2023-11-10 14:33:26 +0100

    all mandatories

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
+	}
+	if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	if (n >= 0 && n <= 9)
+	{
+		ft_putchar_fd(n + '0', fd);
+		return ;
+	}
+}
